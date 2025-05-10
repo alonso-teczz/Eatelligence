7e1166f2d70f543ac137238e43077978ea01ec5d
alonso-teczz <diezgarciaalonso@gmail.com>
2025-05-10 16:26:54 +0200

    Cambio en la app ejecutable

--- src/main/java/com/alonso/eatelligence/EatelligenceApplication.java
+++ src/main/java/com/alonso/eatelligence/EatelligenceApplication.java
@@ -1,11 +1,7 @@
 package com.alonso.eatelligence;
 
-import java.io.IOException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class EatelligenceApplication {
@@ -19,20 +15,20 @@ public class EatelligenceApplication {
     }
 
 	//! Ejecuta el navegador automáticamente cuando se inicie la aplicacion
-	@EventListener(ApplicationReadyEvent.class)
-    public void openBrowser() {
-        String url = "http://localhost:8080";
-        String os = System.getProperty("os.name").toLowerCase();
-        try {
-            if (os.contains("win")) {
-                Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
-            } else if (os.contains("mac")) {
-                Runtime.getRuntime().exec("open " + url);
-            } else if (os.contains("nix") || os.contains("nux")) {
-                Runtime.getRuntime().exec("xdg-open " + url);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	// @EventListener(ApplicationReadyEvent.class)
+    // public void openBrowser() {
+    //     String url = "http://localhost:8080";
+    //     String os = System.getProperty("os.name").toLowerCase();
+    //     try {
+    //         if (os.contains("win")) {
+    //             Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+    //         } else if (os.contains("mac")) {
+    //             Runtime.getRuntime().exec("open " + url);
+    //         } else if (os.contains("nix") || os.contains("nux")) {
+    //             Runtime.getRuntime().exec("xdg-open " + url);
+    //         }
+    //     } catch (IOException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
 }
