c4deb1fa13cf11ee5b44f987cb3aa43f3017a36d
alonso-teczz <diezgarciaalonso@gmail.com>
2025-05-13 17:15:44 +0200

    Elimino primero

--- src/main/java/com/alonso/eatelligence/service/IEntitableClient.java
+++
@@ -1,8 +0,0 @@
-package com.alonso.eatelligence.service;
-
-import com.alonso.eatelligence.model.dto.ClienteRegistroDTO;
-import com.alonso.eatelligence.model.entity.Usuario;
-
-public interface IEntitableClient {
-    public Usuario clientDTOtoEntity(ClienteRegistroDTO cliente);
-}
